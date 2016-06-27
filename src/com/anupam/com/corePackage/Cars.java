94910fdfcec0730507d2a9661f757f564df9c2d6
anupamadak <anupam5659@live.com>
2016-06-27 18:24:04 +0530

    Signed-off-by: anupamadak <anupam5659@live.com>

---
+++ src/com/anupam/com/corePackage/Cars.java
@@ -0,0 +1,10 @@
+package com.anupam.com.corePackage;
+
+public class Cars {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+
+	}
+
+}
---
+++ src/myTest/DefaultSizes.java
@@ -0,0 +1,16 @@
+package myTest;
+
+public class DefaultSizes {
+	int i ;
+	Integer I;
+	String s ;
+	public static void main(String[] args) {
+	
+	System.out.println();
+		
+		
+		
+
+	}
+
+}
